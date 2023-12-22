7ed35aee045167c133bed77d87987bb8898b0134
NGMS Ariyarathna <129283828+NemesisPrasa@users.noreply.github.com>
2023-12-22 17:38:44 +0530

    daily updated

--- pages/chartData.js
+++ pages/chartData.js
@@ -174,3 +174,140 @@ const barChart = new Chart(ctx, {
     },
     }
 });
+
+
+const chartDataBornToBe = {
+  labels: ['12.19', '12.20', '12.21', '12.22',],
+  datasets: [
+    {
+      label: 'BTB',
+      data: [ 3265045, 4666849, 5543391, 6150551],
+      borderColor: '#00ffb7',   // Green color for the line
+      backgroundColor: 'rgba(0, 255, 183, 0.3)',   // Green fill with transparency
+      borderWidth: 1.5,           // Adjust the line thickness
+      fill: false,               // Fill the area under the line
+      tension: 0.4,
+      pointRadius: 0,// Adjust the line curve (0 is straight, 1 is very curved)
+    },
+
+    {
+      label: 'COMH',
+      data: [ 0, 0, 1379276, 1829514],
+      borderColor: '#ff6384',   // Green color for the line
+      backgroundColor: 'rgba(255, 99, 132, 0.3)',   // Green fill with transparency
+      borderWidth: 1.5,           // Adjust the line thickness
+      fill: false,               // Fill the area under the line
+      tension: 0.4,
+      pointRadius: 0,// Adjust the line curve (0 is straight, 1 is very curved)
+    },
+
+
+    // Add more datasets for other videos as needed
+  ],
+};
+
+const chartConfigBornToBe = {
+  type: 'line',
+  data: chartDataBornToBe,
+  options: {
+    legend: {
+      display: false,
+      tooltips: {
+      enabled: false, // Set this to false to hide tooltips
+    },
+      labels: {
+        // Customize the legend label appearance
+        usePointStyle: false, // Do not use point style (square)
+        fontColor: 'white', // Set the font color to match the line color
+      },
+    
+    },
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: false,
+          text: 'Time Period',
+          align: 'end',
+        },
+        ticks: {
+          color: 'white',
+          align: 'end', // Change the X axis labels color
+        },
+      },
+      y: {
+        display: true,
+        title: {
+          display: false,
+          align: 'center',
+          text: 'Daily Views',
+          
+        },
+        ticks: {
+          color: 'white', // Change the Y axis labels color
+        },
+        min: 0,
+      },
+    },
+  },
+};
+
+const BornToBe = new Chart(document.getElementById('ChartBornToBe'), chartConfigBornToBe);
+
+
+const days = ["D1", "D2", "D3", "D4"];
+const BTBViews = [3265045,1401804, 876542, 607160];
+const COMHViews = [1379206, 450238, 0, 0];
+
+
+
+// Get the canvas element and create a chart
+const ctxBTB = document.getElementById("barChartBTB").getContext("2d");
+const barChartBTB = new Chart(ctxBTB, {
+    type: "bar",
+    data: {
+        labels: days,
+        datasets: [
+            {
+                label: "BTB",
+                data: BTBViews,
+                backgroundColor: "rgba(75, 192, 192, 0.6)",
+                borderColor: "rgba(75, 192, 192, 1)",
+                borderWidth: 1
+            },
+            {
+              label: "COMH",
+              data: COMHViews,
+              backgroundColor: "rgba(255, 99, 132, 0.6)",
+                borderColor: "rgba(255, 99, 132, 1)",
+              borderWidth: 1
+          }
+            
+        ]
+    },
+    options: {
+        scales: {
+      x: {
+        display: true,
+        title: {
+          display: false,
+          text: 'Time Period',
+        },
+        ticks: {
+          color: 'white', // Change the X axis labels color
+        },
+      },
+      y: {
+        display: true,
+        title: {
+          display: false,
+          text: 'Daily Views',
+        },
+        ticks: {
+          color: 'white', // Change the Y axis labels color
+        },
+        min: 0,
+      },
+    },
+    }
+});
